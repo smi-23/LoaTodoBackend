96653e6033313302e68d3f5faf0f742241d25327
smi-23 <latack789@naver.com>
2024-06-03 07:58:12 +0900

    Initial commit

---
+++ src/test/java/com/loatodo/loatodobackend/LoaTodoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loatodo.loatodobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoaTodoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
