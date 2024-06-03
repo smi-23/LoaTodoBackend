a5fe07e84a28f094c93a5ddf93a39a6f3e68ba9c
smi-23 <latack789@naver.com>
2024-06-03 18:54:17 +0900

    chore: yml파일 수정

---
+++ src/main/java/com/loatodo/loatodobackend/HelloController.java
@@ -0,0 +1,12 @@
+package com.loatodo.loatodobackend;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/")
+    public String Hello() {
+        return "Hello World";
+    }
+}
