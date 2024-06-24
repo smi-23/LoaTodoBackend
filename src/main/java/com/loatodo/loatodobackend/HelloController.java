43300166cf00c39943eb8a9c3d6391cdfd73fc8e
smi-23 <latack789@naver.com>
2024-06-24 14:28:38 +0900

    test: ec2 인스턴스 변경 후 배포 테스6

--- src/main/java/com/loatodo/loatodobackend/HelloController.java
+++ src/main/java/com/loatodo/loatodobackend/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @RequestMapping("/")
     public String Hello() {
-        return "http 배포 테스트 중입니다.6/23 %불포함 db ip 적용 일단 배포는 됨";
+        return "http 배포 테스트 중입니다.6/23 %불포함 db ip 적용 일단 배포는 됨 바로 종료가 되기 때문에 로그 확인 ";
     }
 }
