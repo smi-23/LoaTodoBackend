7e0ed45206246320764bfbe4f0a5bfe57e38a75e
smi-23 <latack789@naver.com>
2024-07-29 17:59:31 +0900

    feat: env설정 배포 테스트

--- src/main/java/com/loatodo/loatodobackend/HelloController.java
+++ src/main/java/com/loatodo/loatodobackend/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @RequestMapping("/")
     public String Hello() {
-        return "sudo docker-compose down 후 재실행";
+        return "sudo docker-compose down 후 재실 후 잘되는 것 확인 다시 한번 테스트";
     }
 }
