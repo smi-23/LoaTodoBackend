60ea8fdf4f71afde44c7f0336d750e2e56075bf1
smi-23 <latack789@naver.com>
2024-07-29 17:40:23 +0900

    feat: env설정 배포 테스트

--- src/main/java/com/loatodo/loatodobackend/HelloController.java
+++ src/main/java/com/loatodo/loatodobackend/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @RequestMapping("/")
     public String Hello() {
-        return "도커 컴포즈 배포 테스트 현재 redirect uri는 로컬값";
+        return "도커 컴포즈 배포 테스트 현재 redirect uri는 로컬값1";
     }
 }
